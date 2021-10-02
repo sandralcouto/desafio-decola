36216a5aaa27db19e2a2c8060501ae2be9ee6049
gabsgc <gabriellagarccia@gmail.com>
2021-10-02 12:45:57 -0300

    Criando componente AppTopBar

---
+++ src/components/shared/AppTopBar.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="app-top-bar">
+        <v-app-bar color="amber accent-4" dark>
+            <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+            <v-toolbar-title>Projeto Time 5</v-toolbar-title>
+        </v-app-bar>
+         
+        <v-navigation-drawer v-model="drawer" absolute temporary>
+            <v-list nav dense>
+                <v-list-item-group v-model="group" active-class="amber--text text--accent-4">
+                    <router-link to="/">
+                        <v-list-item>
+                            <v-list-item-icon>
+                                <v-icon>mdi-home</v-icon>
+                            </v-list-item-icon>
+                            <v-list-item-title>Home</v-list-item-title>
+                        </v-list-item>
+                    </router-link>
+                    <v-list-item>
+                        <v-list-item-icon>
+                        <v-icon>mdi-television</v-icon>
+                        </v-list-item-icon>
+                        <v-list-item-title>Animes</v-list-item-title>
+                    </v-list-item>
+                    <v-list-item>
+                        <v-list-item-icon>
+                        <v-icon>mdi-controller-classic</v-icon>
+                        </v-list-item-icon>
+                        <v-list-item-title>Jogos</v-list-item-title>
+                    </v-list-item>
+                    <v-list-item>
+                        <v-list-item-icon>
+                        <v-icon>mdi-lifebuoy</v-icon>
+                        </v-list-item-icon>
+                        <v-list-item-title>Setembro Amarelo</v-list-item-title>
+                    </v-list-item>
+                    <router-link to="/about">
+                        <v-list-item>
+                            <v-list-item-icon>
+                            <v-icon>mdi-information</v-icon>
+                            </v-list-item-icon>
+                            <v-list-item-title>Sobre</v-list-item-title>
+                        </v-list-item>
+                    </router-link>
+                </v-list-item-group>
+            </v-list>
+        </v-navigation-drawer>
+     </div>
+</template>
+
+<script>
+export default {
+    name: 'AppTopBar',
+    data: () => ({
+        drawer: false,
+        group: null
+    }),
+}
+</script>
+
+<style scoped>
+.app-top-bar a {
+    text-decoration: none;
+}
+</style>
